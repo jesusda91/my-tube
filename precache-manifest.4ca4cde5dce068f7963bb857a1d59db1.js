093b71c3e734beacd4122b5c7382ce4e3a69518b
Jesus Martinez <jesus.martinez@vecindario.com>
2020-10-18 11:21:38 -0500

    Updates

---
+++ precache-manifest.4ca4cde5dce068f7963bb857a1d59db1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097df88ec73af2ec6df0a1af86eb4e1f",
+    "url": "/my-tube/index.html"
+  },
+  {
+    "revision": "b4807ab7b9f570caf489",
+    "url": "/my-tube/static/css/4.02eeb3e9.chunk.css"
+  },
+  {
+    "revision": "a45a9fa68c88dd91c909",
+    "url": "/my-tube/static/css/5.885d91a7.chunk.css"
+  },
+  {
+    "revision": "b32b905d7c1b047cc9b4",
+    "url": "/my-tube/static/css/6.fcf4f6f3.chunk.css"
+  },
+  {
+    "revision": "6f14ef49f55b2c4cb171",
+    "url": "/my-tube/static/css/7.fcf4f6f3.chunk.css"
+  },
+  {
+    "revision": "a66e62de96fa2c782e78",
+    "url": "/my-tube/static/css/8.fcf4f6f3.chunk.css"
+  },
+  {
+    "revision": "0b97df105421f7ef99c7",
+    "url": "/my-tube/static/css/main.5958ffd3.chunk.css"
+  },
+  {
+    "revision": "b17246b31657528769d6",
+    "url": "/my-tube/static/js/0.3f493ac0.chunk.js"
+  },
+  {
+    "revision": "5b6a3f1ca6e193597e4d49c7a42851aa",
+    "url": "/my-tube/static/js/0.3f493ac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a9ac905024463be9eb8",
+    "url": "/my-tube/static/js/3.1beaf07f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/my-tube/static/js/3.1beaf07f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4807ab7b9f570caf489",
+    "url": "/my-tube/static/js/4.0c62107e.chunk.js"
+  },
+  {
+    "revision": "a45a9fa68c88dd91c909",
+    "url": "/my-tube/static/js/5.c1b52e94.chunk.js"
+  },
+  {
+    "revision": "b32b905d7c1b047cc9b4",
+    "url": "/my-tube/static/js/6.b65cbc04.chunk.js"
+  },
+  {
+    "revision": "6f14ef49f55b2c4cb171",
+    "url": "/my-tube/static/js/7.e303d388.chunk.js"
+  },
+  {
+    "revision": "a66e62de96fa2c782e78",
+    "url": "/my-tube/static/js/8.f238e48c.chunk.js"
+  },
+  {
+    "revision": "0b97df105421f7ef99c7",
+    "url": "/my-tube/static/js/main.75a5e578.chunk.js"
+  },
+  {
+    "revision": "f2fbba727ced51f71a7d",
+    "url": "/my-tube/static/js/runtime-main.63e22ab4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-tube/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "319c48f8c950f754d98b56245534aa80",
+    "url": "/my-tube/static/media/mytube.319c48f8.png"
+  }
+]);
